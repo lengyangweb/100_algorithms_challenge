b1af26a97ede9ed0bff020826d17c1fb736e908b
Leng Yang <lengyang.web@gmail.com>
2025-01-21 23:54:54 -0600

    start addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,19 @@
 export function addBorder(picture: string[]): string[] {
+    const borders = "*****";
+    let pictures: string[] = [];
+    if (picture.some((el) => el.includes(','))) {
 
+    }
+    pictures = picture.map((picture) => {
+        if (picture.includes(',')) {
+            return picture.split(',').map((el) => `*${el.trim()}*`)
+        } else {
+            return `*${picture}*`
+        }
+    });
+    pictures.unshift(borders);
+    pictures.push(borders);
+    return pictures;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
