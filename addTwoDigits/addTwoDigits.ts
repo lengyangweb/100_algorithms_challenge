c695a84a04b36ec1e6f1a62cb0f4da6c18d13331
Leng Yang <lengyang.web@gmail.com>
2025-01-22 00:17:21 -0600

    solve addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,6 @@
 export function addTwoDigits(n: any): number {
-
+    const nums = n.toString().split('');
+    return nums.reduce((acc: number, num: string) => acc += parseInt(num), 0);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
