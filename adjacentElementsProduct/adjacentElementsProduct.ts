321dbdc5165c4f59d5c6035e8f6ea1c28fedb7dd
Leng Yang <lengyang.web@gmail.com>
2025-01-22 00:30:07 -0600

    solve adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+    let largest = 0;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        const a = inputArray[i];
+        const b = inputArray[i+1];
+        const sum = a * b;
+        largest = sum > largest ? sum : largest;
+    }
+    return largest;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
